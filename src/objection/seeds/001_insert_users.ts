78a3f682016a9a39d6583048e66a14bb0108154c
Mari Spirandelli <mariannaspirandelli@gmail.com>
2022-11-03 08:32:19 -0300

    adding user seed file

---
+++ src/objection/seeds/001_insert_users.ts
@@ -0,0 +1,36 @@
+import { Knex } from 'knex';
+import { generateUserData } from '../helpers/generateUserData';
+
+exports.seed = async (knex: Knex): Promise<any> => {
+  const amountToExecuteInParallel = 1000;
+  const numberOfRowsArray = [...new Array(amountToExecuteInParallel)];
+
+  let savedRows = 0;
+  while (savedRows < 1000100) {
+    const usersRows = [];
+    // executes in parallel each array item
+    numberOfRowsArray.map(() => {
+      const { name, username, type, email, bio, avatar } = generateUserData();
+      usersRows.push({
+        name,
+        username,
+        type,
+        email,
+        bio,
+        avatar,
+        created_at: new Date().toISOString(),
+      });
+    });
+  
+    try {
+      await knex('users')
+          .insert(usersRows)
+          .returning('id');
+  
+      savedRows += amountToExecuteInParallel;
+    } catch (error) {
+      // tslint:disable-next-line: no-console
+      console.log(`[ERROR]: Error while seeding database due to `, error.message)
+    }
+  }
+};
