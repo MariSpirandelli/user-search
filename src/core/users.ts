43eb9f30fa2f18ec34d262ba16e5e489c6473725
Mari Spirandelli <mariannaspirandelli@gmail.com>
2022-11-03 08:40:45 -0300

    addig cache to user search service

--- src/core/users.ts
+++ src/core/users.ts
@@ -1,6 +1,11 @@
-import { QueryBuilder } from 'objection';
+import bunyan from 'bunyan';
+import crypto from 'crypto';
+import { Page } from 'objection';
+import cache from '../cache';
 import { User } from '../objection/models/user';
 import { UserFilter, UserInput } from '../types/user';
+
+const logger = bunyan.createLogger({ name: 'core::users' });
 
 export default class Users {
   static async create(user: UserInput) {
@@ -13,14 +18,55 @@ export default class Users {
     return User.query().update(userData).where('id', id).returning('*');
   }
 
+  /**
+   * Get user by Id
+   * @param id
+   * @returns
+   */
   static get(id: number) {
     return User.query().where('id', id).first();
   }
 
-  
-  static getUsers(filters: UserFilter): QueryBuilder<User, User[]> {
-    return User.query().where('id', filters.search).select();
-  }
+  /**
+   * Get all users by pages attending filters specifications
+   * @param filters
+   * @returns
+   */
+  static async getUsers(filters: UserFilter): Promise<Page<User>> {
+    const key = crypto
+      .createHash('md5')
+      .update(JSON.stringify(filters))
+      .digest('hex');
+    const cached = await cache.getByKey(key);
+    if (cached) {
+      logger.info(`Cached value found. Returning cached...`);
 
-  
+      return JSON.parse(cached);
+    }
+    const { page, pageSize, type, search } = filters;
+    logger.info(`Cached value NOT found. Loading from DB...`);
+    const queryBuilder = User.query().select().page(page, pageSize);
+
+    if (type) {
+      queryBuilder.where('type', type);
+    }
+
+    if (search) {
+      const searchTerm = `%${search.toLowerCase()}%`;
+      queryBuilder.where((searchBuilder) =>
+        searchBuilder
+          .whereRaw('LOWER(name) ilike ?', searchTerm)
+          .orWhere('email', 'ilike', searchTerm)
+          .orWhereRaw('LOWER(username) ilike ?', searchTerm)
+      );
+    }
+
+    const newCacheResult = await queryBuilder;
+
+    logger.info(`Caching results...`);
+    await cache.set(key, JSON.stringify(newCacheResult));
+    logger.info(`Returning results...`);
+
+    return newCacheResult;
+  }
 }
